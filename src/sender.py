ccf8843d5a9fa5fcee761fd1d9e52a4127b9f1bd
SHAIK MAHMMAD SIRAZ <shaiksiraaj2004@gmail.com>
2026-07-08 16:10:25 +0530

    Update sender.py

--- src/sender.py
+++ src/sender.py
@@ -4,21 +4,20 @@ from config import EMAIL_USER, EMAIL_PASSWORD, RECIPIENT_EMAIL
 
 
 def send_email(subject, body):
-    try:
-        msg = EmailMessage()
-        msg["Subject"] = subject
-        msg["From"] = EMAIL_USER
-        msg["To"] = RECIPIENT_EMAIL
-        msg.set_content(body)
+    msg = EmailMessage()
+    msg["Subject"] = subject
+    msg["From"] = EMAIL_USER
+    msg["To"] = RECIPIENT_EMAIL
+    msg.set_content(body)
 
+    try:
         with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
             smtp.login(EMAIL_USER, EMAIL_PASSWORD)
             smtp.send_message(msg)
-
-        print("✅ Email sent successfully!")
+        print("Email sent successfully!")
 
     except Exception as e:
-        print(f"❌ Email sending failed: {e}")
+        print(f"Email sending failed: {e}")
 
 
 if __name__ == "__main__":
